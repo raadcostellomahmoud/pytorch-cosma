3e3bcd83a671039e3893cebd894433fdd4112bca
Ra'ad Costello-Mahmoud <mahmoud.raad@yahoo.co.uk>
2025-02-04 16:42:23 +0000

    Fix model_class retrieval in CIFAR-10 ViT test setup

--- unit_testing/examples/test_cifar10_vit.py
+++ unit_testing/examples/test_cifar10_vit.py
@@ -20,7 +20,7 @@ class TestCIFAR10ViT(unittest.TestCase):
         # Load configuration and model
         cls.raw_config = YamlParser("configs/example_vit_model.yaml").parse()
         cls.validated_config = ConfigModel(**cls.raw_config).to_dict()
-        model_class = cls.validated_config.pop("model_class")
+        model_class = globals()[cls.validated_config.pop("model_class")]
         cls.model = model_class(cls.validated_config, device=cls.device)
 
         # Training Setup
